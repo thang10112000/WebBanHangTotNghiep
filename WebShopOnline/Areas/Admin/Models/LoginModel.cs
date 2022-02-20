b95f9aa81dfad2e30bd921a862d8601a50905824
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2022-02-20 10:08:34 +0700

    tạo trang login

---
+++ WebShopOnline/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebShopOnline.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập tài khoản")]
+        public string UserName { set; get; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        public string Password { set; get; }
+
+        public bool RememberMe { set; get; }
+    }
+}
