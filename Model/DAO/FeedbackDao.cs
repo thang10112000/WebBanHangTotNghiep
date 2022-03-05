8aed931f7f3619d170f6f83d0867b0703c660435
Thắng nè <45776208+thang10112000@users.noreply.github.com>
2022-03-05 08:33:33 +0700

    up

---
+++ Model/DAO/FeedbackDao.cs
@@ -0,0 +1,45 @@
+﻿using Model.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.DAO
+{
+    public class FeedbackDao
+    {
+        ShopOnline db = null;
+        public FeedbackDao()
+        {
+            db = new ShopOnline();
+
+        }
+      
+        public IEnumerable<Feedback> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Feedback> model = db.Feedbacks;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString) );
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+        public bool Delete(int id)
+        {
+            try
+            {
+                var feedback = db.Feedbacks.Find(id);
+                db.Feedbacks.Remove(feedback);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ WebShopOnline/Areas/Admin/Controllers/FeedbackController.cs
@@ -0,0 +1,28 @@
+﻿using Model.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebShopOnline.Areas.Admin.Controllers
+{
+    public class FeedbackController : BaseController
+    {
+        // GET: Admin/Feedback
+        [HasCredential(RoleID = "VIEW_USER")]
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new FeedbackDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+        [HasCredential(RoleID = "DELETE_USER")]
+        public ActionResult Delete(int id)
+        {
+            new FeedbackDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
